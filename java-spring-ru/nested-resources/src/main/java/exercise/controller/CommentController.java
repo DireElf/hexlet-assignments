785d25fa2857468dd5b6982cb814520c01abe3c2
@hexlet/cli <support@hexlet.io>
2022-11-02 13:14:51 +0700

    submit java-spring-ru/nested-resources

---
+++ java-spring-ru/nested-resources/src/main/java/exercise/controller/CommentController.java
@@ -0,0 +1,72 @@
+package exercise.controller;
+
+import exercise.model.Comment;
+import exercise.repository.CommentRepository;
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+import exercise.ResourceNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+
+
+@RestController
+@RequestMapping("/posts")
+public class CommentController {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    // BEGIN
+    @GetMapping(path = "/{postId}/comments")
+    public Iterable<Comment> getComments(@PathVariable(name = "postId") Long postId) {
+        return commentRepository.findAllByPostId(postId);
+    }
+
+    @GetMapping(path = "/{postId}/comments/{commentId}")
+    public Comment getComment(@PathVariable("postId") long postId, @PathVariable("commentId") long id) {
+        return commentRepository.findByIdAndPostId(id, postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Comment not found"));
+    }
+
+    @PostMapping(path = "/{postId}/comments")
+    public void createComment(@PathVariable("postId") long postId, @RequestBody Comment comment) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Post not found"));
+        comment.setPost(post);
+        commentRepository.save(comment);
+    }
+
+    @PatchMapping("/{postId}/comments/{commentId}")
+    public void updateComment(
+            @PathVariable("postId") long postId,
+            @PathVariable("commentId") long commentId,
+            @RequestBody Comment comment
+    ) {
+        Comment oldComment = commentRepository.findByIdAndPostId(commentId, postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Comment not found"));
+        oldComment.setContent(comment.getContent());
+        commentRepository.save(comment);
+    }
+
+    @DeleteMapping("/{postId}/comments/{commentId}")
+    public void deleteComment(
+            @PathVariable("postId") long postId,
+            @PathVariable("commentId") long commentId
+    ) {
+        Comment comment = commentRepository.findByIdAndPostId(commentId, postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Comment not found"));
+        commentRepository.delete(comment);
+    }
+    // END
+}
